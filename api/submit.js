783223c75f140e014a5650c7fd09d500164eae89
Guilherme Loureiro <guilherme631999@gmail.com>
2024-11-24 14:22:35 +0000

    Using Gemini to recomend side quests

--- api/submit.js
+++ api/submit.js
@@ -1,12 +1,16 @@
 const express = require('express');
-//const helmet = require('helmet');
 const { MongoClient } = require('mongodb');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const app = express();
 
 // Connection URL and Database Name
-const uri = process.env.ConnectionString;  // Replace with your MongoDB connection string
+const uri = process.env.CONNECTION_STRING;  // Replace with your MongoDB connection string
 const dbName = "SideQuest";  // Replace with your desired database name
+
+// SetUp Gemini
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 // POST route to handle form submission
 app.post('/api/submit', async (req, res) => {
@@ -33,8 +37,12 @@ app.post('/api/submit', async (req, res) => {
             submittedAt: new Date()  // Add the current date/time to track submissions
         });
 
+        // Set up question and answer
+        const prompt = "My interests are the following: " + interests + " get me a list of 3 items each to do in " + location;
+        const inputText = await model.generateContent(prompt);
+        
         // Redirect to a thank-you page after successful form submission
-        res.send('Thank you for submitting the form!');
+        res.send('<h2>Side Quest:</h2> <br/> <br/> ' + inputText.response.text().replaceAll('\n','<br/>'));
 
     } catch (error) {
         console.error("An error occurred:", error);
